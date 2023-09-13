62402f19aa657ab8930f0ea6e6d887a200eb2b97
Maximilian Schwalenberg <maximilian.schwalenberg@protonmail.com>
2023-09-13 13:02:57 +0200

    fixed main and relative imports

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-from datacreator import datacreator
+from code.datacreator import datacreator
 
 obj = datacreator(False)
 
 obj.openfile("data/dialog_acts.dat")
 
 obj.assignClass()
-
-obj.preprocessData()
-
-#print(obj.labeled_data)
-
+obj.createDataset()
