9671dd83fbdf40ac471598a2df7d0a8a5a6f4f91
Melissasssss <melissa10293847@hotmail.com>
2023-09-14 13:54:38 +0200

    Update baseline.py

--- source/baseline.py
+++ source/baseline.py
@@ -2,6 +2,20 @@ from source.datacreator import datacreator
 from source.model import Model
 import json
 
+import keyboard
+
+
+talking = True
+
+obj = datacreator(False)
+
+obj.openfile("dialog_acts.dat")
+
+obj.assignClass()
+obj.createDataset()
+
+
+
 class Baseline(Model):
 
     def __init__(self, datacreator_instance: datacreator) -> None:
@@ -36,4 +50,82 @@ class RuleBasedBaseline (Model):
                     break
             counter += 1
         print(correct_counter / counter)
+
+
+
     
+
+def majorityCounter(instance):
+   ack= 0
+   affirm = 0
+   bye = 0
+   confirm = 0
+   deny = 0
+   hello = 0
+   inform = 0
+   negate = 0
+   null = 0
+   repeat = 0
+   reqalts = 0
+   reqmore = 0
+   request = 0 
+   restart = 0
+   for x in obj.y_train:
+       if x == "ack":
+           ack += 1
+       if x == "affirm":
+           affirm += 1
+       if x == "bye":
+           bye += 1
+       if x == "confirm":
+           confirm += 1
+       if x == "deny":
+            deny += 1
+       if x == "hello":
+            hello += 1
+       if x == "inform":
+            inform += 1
+       if x == "negate":
+            negate += 1             
+       if x == "null":
+            null += 1            
+       if x == "repeat":
+            repeat += 1       
+       if x == "reqalts":
+            reqalts += 1
+       if x == "reqmore":
+            reqmore += 1
+       if x == "request":
+            request += 1
+       if x == "restart":
+            restart += 1     
+   
+   dictionary = {"ack": ack,"affirm": affirm, "bye": bye, "confirm": confirm, "deny": deny, "hello": hello, "inform":
+     inform, 
+     "negate":
+          negate , 
+          "null":
+               null , 
+               "reqalts":
+                    reqalts, 
+                    "reqmore":
+                         reqmore, 
+                         "request":
+                              request, 
+                              "restart":
+                                   restart,
+                                   "repeat":
+                                        repeat} 
+   majority = max(dictionary, key=dictionary.get) 
+   
+   return majority
+
+
+                 
+   
+while talking == True:                                                     
+    input("What kind of utterance are you:")
+    print("This utterance is a " + str(majorityCounter(obj)))
+    if keyboard.is_pressed('esc'):
+        talking = False
+        break
