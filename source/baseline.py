60c4a3e918b5b2bb826ff8d4baec17c6d8186f59
Tobias van den Hoogen <TobiasvandenHoogen>
2023-09-15 16:27:58 +0200

    Finished rule-based baseline

--- source/baseline.py
+++ source/baseline.py
@@ -34,22 +34,27 @@ class RuleBasedBaseline(Model):
             if utterance in rule["keywords"]:
                 return rule["intent"]
 
-    def test(self):
+    def evaluate(self, test_data = True):
         counter = 0.0
         correct_counter = 0.0
-        ack_counter = 0.0
-        ack_correct = 0.0
-        for utterance, dialog_act in zip(
-            self.datacreator_instance.x_train, self.datacreator_instance.y_train
-        ):
+        input_set = []
+
+        if test_data:
+            input_set = zip(self.datacreator_instance.x_test,
+                            self.datacreator_instance.y_test)
+        else:
+            input_set = zip(self.datacreator_instance.x_train,
+                            self.datacreator_instance.y_train)
+
+        for utterance, dialog_act in input_set:
             for rule in self.rule_data["rules"]:
-                if (
-                    any(keyword in utterance for keyword in rule["keywords"])
-                ) and dialog_act in rule["intent"]:
+                if any(keyword in utterance for keyword in rule["keywords"]) and dialog_act in rule["intent"]:
                     correct_counter += 1
                     break
             counter += 1
-        print(correct_counter / counter)
+        print("Rule-based baseline accuracy: ", str(round(correct_counter / counter * 100, 2)) + "%")
+
+        
 
 
 def majorityCounter(instance):
@@ -123,4 +128,4 @@ def majorityCounter(instance):
 #     print("This utterance is a " + str(majorityCounter(obj)))
 #     if keyboard.is_pressed('esc'):
 #         talking = False
-#         break
+#  
