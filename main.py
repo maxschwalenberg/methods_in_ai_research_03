7a461a77d811d9ab978ef2236e07676d1d929acf
amagarzogonzalo <amagarzogonzalo@gmail.com>
2023-09-15 14:24:47 +0200

    model.py does the evaluation (instead of each MLmodel)

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from source.ml_model import DecisionTreeModel, LogisticRegressionModel
 
 
 obj = datacreator(False)
-
-obj.openfile("data/dialog_acts.dat")
+filename = 'C:/Users/Alex/Desktop/AI/Workspace/Methods of Research/methods_in_ai_research_03/data/dialog_acts.dat'
+obj.openfile(filename)
 
 obj.assignClass()
 obj.createDataset()
 
 # Rule-Based Baseline
 rule_based_baseline = RuleBasedBaseline(obj)
-rule_based_baseline.loadRulesFile("data/baseline_rules.json")
+filename2 = 'C:/Users/Alex/Desktop/AI/Workspace/Methods of Research/methods_in_ai_research_03/data/baseline_rules.json'
+rule_based_baseline.loadRulesFile(filename2)
 rule_based_baseline.test()
 
 # ML Model 1
@@ -20,31 +21,21 @@ decision_tree = DecisionTreeModel(obj)
 decision_tree.create_bag_of_words()
 decision_tree.fit()
 decision_tree.predict()
-dt_accuracy, dt_precision, dt_recall, dt_class_metrics = decision_tree.evaluate()
-print("Decision Tree Model:")
-print(f"Accuracy: {dt_accuracy:.4f}")
-print(f"Precision: {dt_precision:.4f}")
-print(f"Recall: {dt_recall:.4f}")
-print(f"Metrics per class:")
-for label, metrics in dt_class_metrics.items():
-    print(f"Class {label}:")
-    print(f"  Precision: {metrics['Precision']:.4f}")
-    print(f"  Recall: {metrics['Recall']:.4f}")
+decision_tree.evaluate()
+print("Decision Tree:")
+print("Accuracy: ", decision_tree.accuracy)
+print("Precision: ", decision_tree.precision)
+print("Recall: ", decision_tree.recall)
 
 # ML Model 2
 logistic_regression = LogisticRegressionModel(obj)
 logistic_regression.create_bag_of_words()
 logistic_regression.fit()
 logistic_regression.predict()
-lr_accuracy, lr_precision, lr_recall, lr_class_metrics = logistic_regression.evaluate()
-print("Logistic Regression Model:")
-print(f"Accuracy: {lr_accuracy:.4f}")
-print(f"Precision: {lr_precision:.4f}")
-print(f"Recall: {lr_recall:.4f}")
-print(f"Metrics per class:")
-for label, metrics in lr_class_metrics.items():
-    print(f"{label}:")
-    print(f"  Precision: {metrics['Precision']:.4f}")
-    print(f"  Recall: {metrics['Recall']:.4f}")
+logistic_regression.evaluate()
+print("Logistic Regression:")
+print("Accuracy: ", logistic_regression.accuracy)
+print("Precision: ", logistic_regression.precision)
+print("Recall: ", logistic_regression.recall)
 
 # print(obj.x_train.shape)
--- source/model.py
+++ source/model.py
@@ -1,4 +1,6 @@
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 from source.datacreator import datacreator
+
 
 
 class Model:
@@ -6,8 +8,12 @@ class Model:
         self.datacreator_instance = datacreator_instance
         self.fitted = False
 
-    def evaluate(self):
-        pass
-
     def predict(self):
-        pass
+        self.preds = self.model.predict(self.x_test_encoded)
+
+    def evaluate(self):
+        self.accuracy = accuracy_score(self.datacreator_instance.y_test, self.preds)
+        self.precision = precision_score(self.datacreator_instance.y_test, self.preds, average="micro")
+        self.recall = recall_score(self.datacreator_instance.y_test, self.preds, average='micro')
+    
+      
