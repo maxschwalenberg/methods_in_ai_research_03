112f2fe1d000eb66bff6f60e1d14a4becd38e03a
Maximilian Schwalenberg <maximilian.schwalenberg@protonmail.com>
2023-10-06 04:48:49 +0200

    filepath refactoring

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from source.dialog_management import DialogManagement
-from source.config import load_configuration
+from source.config import load_configuration, load_file_paths_configuration
 from source.datacreator import Datacreator
 from source.ml_model import LogisticRegressionModel
 
 
-filename = "data/dialog_acts.dat"
+filenames_config = load_file_paths_configuration("output/data/file_paths_config.json")
+
 datacreator_with_duplicates = Datacreator(False)
 
 
 # load input data for both datacreator instances
 # and process the data to create the final dataset
-datacreator_with_duplicates.openfile(filename)
+datacreator_with_duplicates.openfile(filenames_config.dialog_acts_path)
 datacreator_with_duplicates.assign_class()
 datacreator_with_duplicates.create_dataset()
 
@@ -21,7 +22,7 @@ logistic_regression.develop()
 logistic_regression.show_results()
 
 
-configuration = load_configuration("output/data/dialog_config.json")
+configuration = load_configuration(filenames_config.dialog_config_path)
 
 
 # create the dialog
--- source/config.py
+++ source/config.py
@@ -1,4 +1,24 @@
 import json
+from dataclasses import dataclass
+
+
+@dataclass
+class FilePathsConfig:
+    dialog_acts_path: str
+    all_dialogs_path: str
+    restaurant_info_path: str
+    extended_restaurant_info_path: str
+    evaluation_results_path: str
+    baseline_rules_path: str
+    additional_requirement_rules_path: str
+    dialog_config_path: str
+
+
+def load_file_paths_configuration(configuration_file_path: str):
+    with open(configuration_file_path) as f:
+        data = json.load(f)
+
+    return FilePathsConfig(**data)
 
 
 def load_configuration(configuration_file_path: str):
@@ -13,3 +33,6 @@ def load_configuration(configuration_file_path: str):
     ]
 
     return configuration
+
+
+load_file_paths_configuration("output/data/file_paths_config.json")
